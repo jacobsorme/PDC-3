5cb248bb5ca520cbbd91ecd1a1a9f0ad48d4f400
Martin Wass <wass@W530.wass>
2019-04-11 17:13:05 +0200

    Implemented omp_sum

--- OpenMP/exercise_2/src/sum.c
+++ OpenMP/exercise_2/src/sum.c
@@ -2,23 +2,24 @@
 
 void omp_sum(double *sum_ret)
 {
-    double sum = 0;
-    int n = omp_get_num_threads();
+    int n = omp_get_max_threads();
     double *res = calloc(n,sizeof(double));
 
-    #pragma omp parallel {
+    #pragma omp parallel
+    {
         int t = omp_get_thread_num();
-        for (int i = t; i < size; i += n){
+        for (int i = t; i < size; i += n) {
             res[t] += x[i];
         }
     }
+    double sum = 0;
     for(int i = 0; i < n; i++) sum += res[i];
     *sum_ret = sum;
 }
 
 void omp_critical_sum(double *sum_ret)
 {
-
+    
 }
 
 void omp_atomic_sum(double *sum_ret)
