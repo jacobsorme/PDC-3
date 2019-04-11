deff805ddcd3228a46f2662c9816335ffcc78f71
Martin Wass <wass@W530.wass>
2019-04-11 17:22:30 +0200

    Implemented omp_critical_sum and omp_atomic_sum

--- OpenMP/exercise_2/src/sum.c
+++ OpenMP/exercise_2/src/sum.c
@@ -19,12 +19,32 @@ void omp_sum(double *sum_ret)
 
 void omp_critical_sum(double *sum_ret)
 {
-    
+    int n = omp_get_max_threads();
+    *sum_ret = 0;
+
+    #pragma omp parallel
+    {
+        int t = omp_get_thread_num();
+        for (int i = t; i < size; i += n) {
+            #pragma omp critical
+            *sum_ret += x[i];
+        }
+    }
 }
 
 void omp_atomic_sum(double *sum_ret)
 {
+    int n = omp_get_max_threads();
+    *sum_ret = 0;
 
+    #pragma omp parallel
+    {
+        int t = omp_get_thread_num();
+        for (int i = t; i < size; i += n) {
+            #pragma omp atomic
+            *sum_ret += x[i];
+        }
+    }
 }
 
 void omp_local_sum(double *sum_ret)
