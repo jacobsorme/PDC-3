6510a4e182f1ad382d9fcd1611c6afd2259710fa
Martin Wass <wass@W530.wass>
2019-04-11 17:34:42 +0200

    Reimplemented omp_sum and moved old to local_sum

--- OpenMP/exercise_2/src/sum.c
+++ OpenMP/exercise_2/src/sum.c
@@ -3,18 +3,15 @@
 void omp_sum(double *sum_ret)
 {
     int n = omp_get_max_threads();
-    double *res = calloc(n,sizeof(double));
+    *sum_ret = 0;
 
     #pragma omp parallel
     {
         int t = omp_get_thread_num();
         for (int i = t; i < size; i += n) {
-            res[t] += x[i];
+            *sum_ret += x[i];
         }
     }
-    double sum = 0;
-    for(int i = 0; i < n; i++) sum += res[i];
-    *sum_ret = sum;
 }
 
 void omp_critical_sum(double *sum_ret)
@@ -49,7 +46,19 @@ void omp_atomic_sum(double *sum_ret)
 
 void omp_local_sum(double *sum_ret)
 {
+    int n = omp_get_max_threads();
+    double *res = calloc(n,sizeof(double));
 
+    #pragma omp parallel
+    {
+        int t = omp_get_thread_num();
+        for (int i = t; i < size; i += n) {
+            res[t] += x[i];
+        }
+    }
+    double sum = 0;
+    for(int i = 0; i < n; i++) sum += res[i];
+    *sum_ret = sum;
 }
 
 void omp_padded_sum(double *sum_ret)
