5bb6b77624bd66c2048646ebc4953f7e3b04e61b
Jacobs Apple ID <sorme@kth.se>
2019-04-11 16:49:00 +0200

    Implement omp_sum in exercise_2

--- OpenMP/exercise_2/src/sum.c
+++ OpenMP/exercise_2/src/sum.c
@@ -2,7 +2,18 @@
 
 void omp_sum(double *sum_ret)
 {
+    double sum = 0;
+    int n = omp_get_num_threads();
+    double *res = calloc(n,sizeof(double));
 
+    #pragma omp parallel {
+        int t = omp_get_thread_num();
+        for (int i = t; i < size; i += n){
+            res[t] += x[i];
+        }
+    }
+    for(int i = 0; i < n; i++) sum += res[i];
+    *sum_ret = sum;
 }
 
 void omp_critical_sum(double *sum_ret)
