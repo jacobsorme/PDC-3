065381f7f98536589d3e91231f729ffcfb747743
Martin Wass <wass@W530.wass>
2019-04-14 19:07:07 +0200

    he

---
+++ Exercise 2/Scripts/figuremaker.m
@@ -0,0 +1,12 @@
+clf;
+clc;
+
+run("Results/matlab_variables.m");
+
+% bar_labels = categorical({'1', '2', '4', '8', '16', '32'});
+% bar_labels = reordercats(bar_labels,{'1', '2', '4', '8', '16', '32'});
+% barh(bar_labels,sum_results_5000(:,[1,4,5,6,7]));
+% xlabel("Time");
+% ylabel("Threads");
+% 
+% legend("serial", "local (array)", "padded", "private (for)", "reduction");
