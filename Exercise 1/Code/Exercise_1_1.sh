bbc2f6dd08729fe49ba2d08e7279e75d08c6e309
Martin Wass <mawass@kth.se>
2019-04-14 16:42:13 +0200

    Benchy

--- Exercise 1/Code/Exercise_1_1.sh
+++ Exercise 1/Code/Exercise_1_1.sh
@@ -9,13 +9,13 @@
 mkdir -p Results_1
 
 # Run the stream benchmark for 1, 2, 4, 8, 16 and 32 threads and save the results in Results_1 directory
-for i in {29..32}
+for i in {1..32}
 do
 	# Run each benchmark five times
 	for j in {1..5}
 	do
 		export OMP_NUM_THREADS=$i
-		aprun -n 1 -d $OMP_NUM_THREADS ./stream.out > Results_1/stream_benchmark_${i}_threads_${j}.txt
+		#aprun -n 1 -d $OMP_NUM_THREADS ./stream.out > Results_1/stream_benchmark_${i}_threads_${j}.txt
 	done
 done
 
@@ -24,7 +24,7 @@ done
 echo "stream_bandwidths = [" > Results_1/matlab_matrix.m
 echo -e "\t% First element of row = number of threads, then comes the benchmark results" >> Results_1/matlab_matrix.m
 
-for i in {29..32}
+for i in {1..32}
 do
 	echo -ne "\t$i" >> Results_1/matlab_matrix.m
 	for j in {1..5}
