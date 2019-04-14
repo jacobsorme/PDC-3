71b35aa30d4d3fdf561c2753dbc8ff99479ef5b7
Martin Wass <mawass@kth.se>
2019-04-14 18:58:20 +0200

    ex2

---
+++ Exercise 2/benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -J Parallel_sum
+#SBATCH -t 00:30:00
+#SBATCH -A edu19.DD2356
+#SBATCH --nodes=1
+#SBATCH -e error_file.e
+
+mkdir -p Results
+
+for i in 5000 30000
+do
+	for j in 1 2 4 8 16 32
+	do
+		export OMP_NUM_THREADS=${j}
+		aprun -n 1 -d $OMP_NUM_THREADS ./bin/sum.out -d -n ${i} -r 50 > Results/sum_results_threads_${j}_size_${i}.txt 	
+	done
+done
---
+++ Exercise 2/extract.sh
@@ -0,0 +1,24 @@
+for i in 5000 30000
+do
+echo "sum_results_$i = [" > Results/matlab_variables.m
+	for j in 1 2 4 8 16 32
+	do
+		echo -en "\t" > Results/matlab_variables.m
+		grep -Poi "Max\(serial\).*duration = \K[0-9]+.[0-9]+" Results/sum_results_threads_${j}_size_${i}.txt | tr -d '\n' > Results/matlab_variables.m
+		echo -en ",\t" > Results/matlab_variables.m
+		grep -Poi "Test\(omp_critical\).*duration = \K[0-9]+.[0-9]+" Results/sum_results_threads_${j}_size_${i}.txt | tr -d '\n' > Results/matlab_variables.m
+		echo -en ",\t" > Results/matlab_variables.m
+		grep -Poi "Test\(omp_atomic\).*duration = \K[0-9]+.[0-9]+" Results/sum_results_threads_${j}_size_${i}.txt | tr -d '\n' > Results/matlab_variables.m
+		echo -en ",\t" > Results/matlab_variables.m
+		grep -Poi "Test\(omp_local\).*duration = \K[0-9]+.[0-9]+" Results/sum_results_threads_${j}_size_${i}.txt | tr -d '\n' > Results/matlab_variables.m
+		echo -en ",\t" > Results/matlab_variables.m
+		grep -Poi "Test\(omp_padded\).*duration = \K[0-9]+.[0-9]+" Results/sum_results_threads_${j}_size_${i}.txt | tr -d '\n' > Results/matlab_variables.m
+		echo -en ",\t" > Results/matlab_variables.m
+		grep -Poi "Test\(omp_private\).*duration = \K[0-9]+.[0-9]+" Results/sum_results_threads_${j}_size_${i}.txt | tr -d '\n' > Results/matlab_variables.m
+		echo -en ",\t" > Results/matlab_variables.m
+		grep -Poi "Test\(omp_reduction\).*duration = \K[0-9]+.[0-9]+" Results/sum_results_threads_${j}_size_${i}.txt | tr -d '\n'  > Results/matlab_variables.m
+		echo ";" > Results/matlab_variables.m
+	done
+	echo "];" > Results/matlab_variables.m
+done
+
