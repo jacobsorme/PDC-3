a1925c8934679048ce03b6baa0af49f4fce09799
Martin Wass <mawass@kth.se>
2019-04-14 16:38:16 +0200

    Bench

--- Exercise 1/Code/Exercise_1_1.sh
+++ Exercise 1/Code/Exercise_1_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -J stream_benchmark
-#SBATCH -t 0:10:00
+#SBATCH -t 0:30:00
 #SBATCH -A edu19.DD2356
 #SBATCH --nodes=1
 #SBATCH -e error_file.e
@@ -9,7 +9,7 @@
 mkdir -p Results_1
 
 # Run the stream benchmark for 1, 2, 4, 8, 16 and 32 threads and save the results in Results_1 directory
-for i in {1..32}
+for i in {29..32}
 do
 	# Run each benchmark five times
 	for j in {1..5}
@@ -24,7 +24,7 @@ done
 echo "stream_bandwidths = [" > Results_1/matlab_matrix.m
 echo -e "\t% First element of row = number of threads, then comes the benchmark results" >> Results_1/matlab_matrix.m
 
-for i in {1..32}
+for i in {29..32}
 do
 	echo -ne "\t$i" >> Results_1/matlab_matrix.m
 	for j in {1..5}
