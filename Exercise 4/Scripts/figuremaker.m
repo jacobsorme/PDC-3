25674b5c3a4af93d0284d3fc877f9f346832145c
Jacob Sorme <sorme@kth.se>
2019-04-15 19:16:10 +0200

    Testing figures exercise 4

--- Exercise 4/Scripts/figuremaker.m
+++ Exercise 4/Scripts/figuremaker.m
@@ -3,17 +3,17 @@ clc;
 
 run("matlab_variables.m");
 x = [1:32];
-y = [50:50:1500];
+y = [50:50:500];
 
 f = figure;
 
-surf(x,y,times_nbody);
+surf(x,y,times_nbody(1:10,:));
 view(45,10);
 
 xlabel("Threads")
 ylabel("Number of bodies (N)")
 zlabel("Time (s)")
 
-saveas(f,"../Figures/surf_ex4_3","svg");
+saveas(f,"../Figures/surf_ex4_4","svg");
 
 
