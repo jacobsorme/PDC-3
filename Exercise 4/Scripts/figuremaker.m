28891ab5bd9afd2dd18505853746d930d5b8a7a2
Jacob <sorme@kth.se>
2019-04-15 17:19:20 +0200

    Add work for matlab exercise 4

---
+++ Exercise 4/Scripts/figuremaker.m
@@ -0,0 +1,19 @@
+clf;
+clc;
+
+run("../Results/matlabvars.m");
+x = [1..32];
+y = [50..50..1500];
+
+f = figure;
+
+surf(x,y,times_nbody);
+view(15,10);
+
+xlabel("X")
+ylabel("Y")
+zlabel("Time (asdad)")
+
+saveas(f,"../Figures/surf_ex4","svg");
+
+
