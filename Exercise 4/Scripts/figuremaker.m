2c600b45e9e675bd91497a0f7a507abceb6f89f6
Jacob Sorme <sorme@kth.se>
2019-04-15 17:36:17 +0200

    Add figures

--- Exercise 4/Scripts/figuremaker.m
+++ Exercise 4/Scripts/figuremaker.m
@@ -3,16 +3,16 @@ clc;
 
 run("matlab_variables.m");
 x = [1:32];
-y = [50:50:1500];
+y = [50:50:1000];
 
 f = figure;
 
-surf(x,y,times_nbody);
+surf(x,y,times_nbody(1:20,:));
 view(15,10);
 
-xlabel("X")
-ylabel("Y")
-zlabel("Time (asdad)")
+xlabel("Threads")
+ylabel("Nuber of bodies (N)")
+zlabel("Time (s)")
 
 saveas(f,"../Figures/surf_ex4","svg");
 
