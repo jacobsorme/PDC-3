c2ba17a2d79c6bd14741d15e93c71d9f14790a36
Jacob Sorme <sorme@kth.se>
2019-04-15 17:47:05 +0200

    More figgs

--- Exercise 4/Scripts/figuremaker.m
+++ Exercise 4/Scripts/figuremaker.m
@@ -8,12 +8,12 @@ y = [50:50:1500];
 f = figure;
 
 surf(x,y,times_nbody);
-view(15,10);
+view(25,10);
 
 xlabel("Threads")
 ylabel("Number of bodies (N)")
 zlabel("Time (s)")
 
-saveas(f,"../Figures/surf_ex4_2","svg");
+saveas(f,"../Figures/surf_ex4_3","svg");
 
 
